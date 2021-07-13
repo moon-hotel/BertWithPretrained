b9a718e39f087d2da5e1e9089507f958ac87f94b
moon-hotel <moon-hotel@hotmail.com>
2021-07-13 21:56:51 +0800

    添加Transformer模块

---
+++ model/BasicBert/MyTransformer.py
@@ -0,0 +1,392 @@
+from torch.nn.init import xavier_uniform_
+import torch.nn.functional as F
+from torch.nn import Parameter
+import torch.nn as nn
+import copy
+import torch
+
+
+class MyTransformer(nn.Module):
+    def __init__(self, d_model=512, nhead=8, num_encoder_layers=6,
+                 num_decoder_layers=6, dim_feedforward=2048, dropout=0.1,
+                 ):
+        super(MyTransformer, self).__init__()
+
+        """
+        :param d_model:  d_k = d_v = d_model/nhead = 64, 模型中向量的维度，论文默认值为 512
+        :param nhead:               多头注意力机制中多头的数量，论文默认为值 8
+        :param num_encoder_layers:  encoder堆叠的数量，也就是论文中的N，论文默认值为6
+        :param num_decoder_layers:  decoder堆叠的数量，也就是论文中的N，论文默认值为6
+        :param dim_feedforward:     全连接中向量的维度，论文默认值为 2048
+        :param dropout:             丢弃率，论文中的默认值为 0.1
+        """
+
+        #  ================ 编码部分 =====================
+        encoder_layer = MyTransformerEncoderLayer(d_model, nhead, dim_feedforward, dropout)
+        encoder_norm = nn.LayerNorm(d_model)
+        self.encoder = MyTransformerEncoder(encoder_layer, num_encoder_layers, encoder_norm)
+
+        # ================ 解码部分 =====================
+        decoder_layer = MyTransformerDecoderLayer(d_model, nhead, dim_feedforward, dropout)
+        decoder_norm = nn.LayerNorm(d_model)
+        self.decoder = MyTransformerDecoder(decoder_layer, num_decoder_layers, decoder_norm)
+
+        self._reset_parameters()
+
+        self.d_model = d_model
+        self.nhead = nhead
+
+    def _reset_parameters(self):
+        r"""Initiate parameters in the transformer model."""
+        """
+        初始化
+        """
+        for p in self.parameters():
+            if p.dim() > 1:
+                xavier_uniform_(p)
+
+    def forward(self, src, tgt, src_mask=None, tgt_mask=None,
+                memory_mask=None, src_key_padding_mask=None,
+                tgt_key_padding_mask=None, memory_key_padding_mask=None):
+        """
+        :param src:   [src_len,batch_size,embed_dim]
+        :param tgt:  [tgt_len, batch_size, embed_dim]
+        :param src_mask:  None
+        :param tgt_mask:  [tgt_len, tgt_len]
+        :param memory_mask: None
+        :param src_key_padding_mask: [batch_size, src_len]
+        :param tgt_key_padding_mask: [batch_size, tgt_len]
+        :param memory_key_padding_mask:  [batch_size, src_len]
+        :return: [tgt_len, batch_size, num_heads * kdim] <==> [tgt_len,batch_size,embed_dim]
+        """
+        memory = self.encoder(src, mask=src_mask, src_key_padding_mask=src_key_padding_mask)
+        # [src_len, batch_size, num_heads * kdim] <==> [src_len,batch_size,embed_dim]
+        output = self.decoder(tgt=tgt, memory=memory, tgt_mask=tgt_mask, memory_mask=memory_mask,
+                              tgt_key_padding_mask=tgt_key_padding_mask,
+                              memory_key_padding_mask=memory_key_padding_mask)
+        return output  # [tgt_len, batch_size, num_heads * kdim] <==> [tgt_len,batch_size,embed_dim]
+
+    def generate_square_subsequent_mask(self, sz):
+        r"""Generate a square mask for the sequence. The masked positions are filled with float('-inf').
+            Unmasked positions are filled with float(0.0).
+        """
+        mask = (torch.triu(torch.ones(sz, sz)) == 1).transpose(0, 1)
+        mask = mask.float().masked_fill(mask == 0, float('-inf')).masked_fill(mask == 1, float(0.0))
+        return mask  # [sz,sz]
+
+
+class MyTransformerEncoderLayer(nn.Module):
+    def __init__(self, d_model, nhead, dim_feedforward=2048, dropout=0.1):
+        super(MyTransformerEncoderLayer, self).__init__()
+        """
+        :param d_model:         d_k = d_v = d_model/nhead = 64, 模型中向量的维度，论文默认值为 512
+        :param nhead:           多头注意力机制中多头的数量，论文默认为值 8
+        :param dim_feedforward: 全连接中向量的维度，论文默认值为 2048
+        :param dropout:         丢弃率，论文中的默认值为 0.1    
+        
+        
+        """
+        self.self_attn = MyMultiheadAttention(d_model, nhead, dropout=dropout)
+
+        # Implementation of Feedforward model
+        self.dropout1 = nn.Dropout(dropout)
+        self.norm1 = nn.LayerNorm(d_model)
+
+        self.linear1 = nn.Linear(d_model, dim_feedforward)
+        self.dropout = nn.Dropout(dropout)
+        self.linear2 = nn.Linear(dim_feedforward, d_model)
+        self.activation = F.relu
+
+        self.dropout2 = nn.Dropout(dropout)
+        self.norm2 = nn.LayerNorm(d_model)
+
+    def forward(self, src, src_mask=None, src_key_padding_mask=None):
+        """
+        :param src: 编码部分的输入，形状为 [src_len,batch_size, embed_dim]
+        :param src_mask:  编码部分输入的padding情况，形状为 [batch_size, src_len]
+        :return:
+        """
+        src2 = self.self_attn(src, src, src, attn_mask=src_mask,
+                              key_padding_mask=src_key_padding_mask, )[0]  # 计算多头注意力
+        # src2: [src_len,batch_size,num_heads*kdim] num_heads*kdim = embed_dim
+        src = src + self.dropout1(src2)  # 残差连接
+        src = self.norm1(src)  # [src_len,batch_size,num_heads*kdim]
+
+        src2 = self.activation(self.linear1(src))  # [src_len,batch_size,dim_feedforward]
+        src2 = self.linear2(self.dropout(src2))  # [src_len,batch_size,num_heads*kdim]
+        src = src + self.dropout2(src2)
+        src = self.norm2(src)
+        return src  # [src_len, batch_size, num_heads * kdim] <==> [src_len,batch_size,embed_dim]
+
+
+class MyTransformerEncoder(nn.Module):
+    def __init__(self, encoder_layer, num_layers, norm=None):
+        super(MyTransformerEncoder, self).__init__()
+        """
+        encoder_layer: 就是包含有多头注意力机制的一个编码层
+        num_layers: 克隆得到多个encoder layers 论文中默认为6
+        norm: 归一化层
+        
+        """
+        self.layers = _get_clones(encoder_layer, num_layers)  # 克隆得到多个encoder layers 论文中默认为6
+        self.num_layers = num_layers
+        self.norm = norm
+
+    def forward(self, src, mask=None, src_key_padding_mask=None):
+        """
+        :param src: 编码部分的输入，形状为 [src_len,batch_size, embed_dim]
+        :param mask:  编码部分输入的padding情况，形状为 [batch_size, src_len]
+        :return:# [src_len, batch_size, num_heads * kdim] <==> [src_len,batch_size,embed_dim]
+        """
+        output = src
+        for mod in self.layers:
+            output = mod(output, src_mask=mask,
+                         src_key_padding_mask=src_key_padding_mask)  # 多个encoder layers层堆叠后的前向传播过程
+        if self.norm is not None:
+            output = self.norm(output)
+        return output  # [src_len, batch_size, num_heads * kdim] <==> [src_len,batch_size,embed_dim]
+
+
+def _get_clones(module, N):
+    return nn.ModuleList([copy.deepcopy(module) for _ in range(N)])
+
+
+class MyTransformerDecoderLayer(nn.Module):
+    def __init__(self, d_model, nhead, dim_feedforward=2048, dropout=0.1):
+        super(MyTransformerDecoderLayer, self).__init__()
+        """
+        :param d_model:         d_k = d_v = d_model/nhead = 64, 模型中向量的维度，论文默认值为 512
+        :param nhead:           多头注意力机制中多头的数量，论文默认为值 8
+        :param dim_feedforward: 全连接中向量的维度，论文默认值为 2048
+        :param dropout:         丢弃率，论文中的默认值为 0.1    
+        """
+        self.self_attn = MyMultiheadAttention(embed_dim=d_model, num_heads=nhead, dropout=dropout)
+        # 解码部分输入序列之间的多头注意力（也就是论文结构图中的Masked Multi-head attention)
+        self.multihead_attn = MyMultiheadAttention(embed_dim=d_model, num_heads=nhead, dropout=dropout)
+        # 编码部分输出（memory）和解码部分之间的多头注意力机制。
+        # Implementation of Feedforward model
+
+        self.linear1 = nn.Linear(d_model, dim_feedforward)
+        self.dropout = nn.Dropout(dropout)
+        self.linear2 = nn.Linear(dim_feedforward, d_model)
+
+        self.norm1 = nn.LayerNorm(d_model)
+        self.norm2 = nn.LayerNorm(d_model)
+        self.norm3 = nn.LayerNorm(d_model)
+        self.dropout1 = nn.Dropout(dropout)
+        self.dropout2 = nn.Dropout(dropout)
+        self.dropout3 = nn.Dropout(dropout)
+
+        self.activation = F.relu
+
+    def forward(self, tgt, memory, tgt_mask=None, memory_mask=None, tgt_key_padding_mask=None,
+                memory_key_padding_mask=None):
+        """
+        :param tgt:  解码部分的输入，形状为 [tgt_len,batch_size, embed_dim]
+        :param memory: 编码部分的输出（memory）, [src_len,batch_size,embed_dim]
+        :param tgt_mask: 注意力Mask输入，用于掩盖当前position之后的信息, [tgt_len, tgt_len]
+        :param memory_mask: 编码器-解码器交互时的注意力掩码，一般为None
+        :param tgt_key_padding_mask: 解码部分输入的padding情况，形状为 [batch_size, tgt_len]
+        :param memory_key_padding_mask: 编码部分输入的padding情况，形状为 [batch_size, src_len]
+        :return:
+        """
+        tgt2 = self.self_attn(tgt, tgt, tgt,  # [tgt_len,batch_size, embed_dim]
+                              attn_mask=tgt_mask,
+                              key_padding_mask=tgt_key_padding_mask)[0]
+        # 解码部分输入序列之间'的多头注意力（也就是论文结构图中的Masked Multi-head attention)
+
+        tgt = tgt + self.dropout1(tgt2)  # 接着是残差连接
+        tgt = self.norm1(tgt)  # [tgt_len,batch_size, embed_dim]
+
+        tgt2 = self.multihead_attn(tgt, memory, memory,  # [tgt_len, batch_size, embed_dim]
+                                   attn_mask=memory_mask,
+                                   key_padding_mask=memory_key_padding_mask)[0]
+
+        # 解码部分的输入经过多头注意力后同编码部分的输出（memory）通过多头注意力机制进行交互
+        tgt = tgt + self.dropout2(tgt2)  # 残差连接
+        tgt = self.norm2(tgt)  # [tgt_len, batch_size, embed_dim]
+
+        tgt2 = self.activation(self.linear1(tgt))  # [tgt_len, batch_size, dim_feedforward]
+        tgt2 = self.linear2(self.dropout(tgt2))  # [tgt_len, batch_size, embed_dim]
+        # 最后的两层全连接
+        tgt = tgt + self.dropout3(tgt2)
+        tgt = self.norm3(tgt)
+        return tgt  # [tgt_len, batch_size, num_heads * kdim] <==> [tgt_len,batch_size,embed_dim]
+
+
+class MyTransformerDecoder(nn.Module):
+    def __init__(self, decoder_layer, num_layers, norm=None):
+        super(MyTransformerDecoder, self).__init__()
+        self.layers = _get_clones(decoder_layer, num_layers)
+        self.num_layers = num_layers
+        self.norm = norm
+
+    def forward(self, tgt, memory, tgt_mask=None, memory_mask=None, tgt_key_padding_mask=None,
+                memory_key_padding_mask=None):
+        """
+        :param tgt: 解码部分的输入，形状为 [tgt_len,batch_size, embed_dim]
+        :param memory: 编码部分最后一层的输出 [src_len,batch_size, embed_dim]
+        :param tgt_mask: 注意力Mask输入，用于掩盖当前position之后的信息, [tgt_len, tgt_len]
+        :param memory_mask: 编码器-解码器交互时的注意力掩码，一般为None
+        :param tgt_key_padding_mask: 解码部分输入的padding情况，形状为 [batch_size, tgt_len]
+        :param memory_key_padding_mask: 编码部分输入的padding情况，形状为 [batch_size, src_len]
+        :return:
+        """
+        output = tgt  # [tgt_len,batch_size, embed_dim]
+
+        for mod in self.layers:  # 这里的layers就是N层解码层堆叠起来的
+            output = mod(output, memory,
+                         tgt_mask=tgt_mask,
+                         memory_mask=memory_mask,
+                         tgt_key_padding_mask=tgt_key_padding_mask,
+                         memory_key_padding_mask=memory_key_padding_mask)
+        if self.norm is not None:
+            output = self.norm(output)
+
+        return output  # [tgt_len, batch_size, num_heads * kdim] <==> [tgt_len,batch_size,embed_dim]
+
+
+class MyMultiheadAttention(nn.Module):
+    """
+    多头注意力机制的计算公式为（就是论文第5页的公式）：
+    .. math::
+        \text{MultiHead}(Q, K, V) = \text{Concat}(head_1,\dots,head_h)W^O
+        \text{where} head_i = \text{Attention}(QW_i^Q, KW_i^K, VW_i^V)
+    """
+
+    def __init__(self, embed_dim, num_heads, dropout=0., bias=True):
+        super(MyMultiheadAttention, self).__init__()
+        """
+        :param embed_dim:   词嵌入的维度，也就是前面的d_model参数，论文中的默认值为512
+        :param num_heads:   多头注意力机制中多头的数量，也就是前面的nhead参数， 论文默认值为 8
+        :param dropout:     
+        :param bias:        最后对多头的注意力（组合）输出进行线性变换时，是否使用偏置
+        """
+        self.embed_dim = embed_dim  # 前面的d_model参数
+        self.head_dim = embed_dim // num_heads  # head_dim 指的就是d_k,d_v
+        self.kdim = self.head_dim
+        self.vdim = self.head_dim
+
+        self.num_heads = num_heads  # 多头个数
+        self.dropout = dropout
+
+        assert self.head_dim * num_heads == self.embed_dim, "embed_dim 除以 num_heads必须为整数"
+        # 上面的限制条件就是论文中的  d_k = d_v = d_model/n_head 条件
+
+        self.q_proj_weight = Parameter(torch.Tensor(embed_dim, embed_dim))  # embed_dim = kdim * num_heads
+        # 这里第二个维度之所以是embed_dim，实际上这里是同时初始化了num_heads个W_q堆叠起来的, 也就是num_heads个头
+        self.k_proj_weight = Parameter(torch.Tensor(embed_dim, embed_dim))  # W_k,  embed_dim = kdim * num_heads
+        self.v_proj_weight = Parameter(torch.Tensor(embed_dim, embed_dim))  # W_v,  embed_dim = vdim * num_heads
+
+        self.out_proj = nn.Linear(embed_dim, embed_dim, bias=bias)
+        # 最后将所有的Z组合起来的时候，也是一次性完成， embed_dim = vdim * num_heads
+        self._reset_parameters()
+
+    def _reset_parameters(self):
+        """
+        以特定方式来初始化参数
+        :return:
+        """
+        xavier_uniform_(self.q_proj_weight)
+        xavier_uniform_(self.k_proj_weight)
+        xavier_uniform_(self.v_proj_weight)
+
+    def forward(self, query, key, value, attn_mask=None, key_padding_mask=None):
+        """
+        在论文中，编码时query, key, value 都是同一个输入， 解码时 输入的部分也都是同一个输入，
+        解码和编码交互时 key,value指的是 memory, query指的是tgt
+        :param query: # [tgt_len, batch_size, embed_dim], tgt_len 表示目标序列的长度
+        :param key:  #  [src_len, batch_size, embed_dim], src_len 表示源序列的长度
+        :param value: # [src_len, batch_size, embed_dim], src_len 表示源序列的长度
+        :param attn_mask: # [tgt_len,src_len] or [num_heads*batch_size,tgt_len, src_len]
+                一般只在解码时使用，为了并行一次喂入所有解码部分的输入，所以要用mask来进行掩盖当前时刻之后的位置信息
+        :param key_padding_mask: [batch_size, src_len], src_len 表示源序列的长度
+        :return:
+        attn_output: [tgt_len, batch_size, embed_dim]
+        attn_output_weights: # [batch_size, tgt_len, src_len]
+        """
+        return multi_head_attention_forward(query, key, value, self.num_heads,
+                                            self.dropout, self.out_proj.weight, self.out_proj.bias,
+                                            training=self.training,
+                                            key_padding_mask=key_padding_mask,
+                                            q_proj_weight=self.q_proj_weight,
+                                            k_proj_weight=self.k_proj_weight,
+                                            v_proj_weight=self.v_proj_weight,
+                                            attn_mask=attn_mask)
+
+
+def multi_head_attention_forward(query,  # [tgt_len,batch_size, embed_dim]
+                                 key,  # [src_len, batch_size, embed_dim]
+                                 value,  # [src_len, batch_size, embed_dim]
+                                 num_heads,
+                                 dropout_p,
+                                 out_proj_weight,  # [embed_dim = vdim * num_heads, embed_dim = vdim * num_heads]
+                                 out_proj_bias,
+                                 training=True,
+                                 key_padding_mask=None,  # [batch_size,src_len/tgt_len]
+                                 q_proj_weight=None,  # [embed_dim,kdim * num_heads]
+                                 k_proj_weight=None,  # [embed_dim, kdim * num_heads]
+                                 v_proj_weight=None,  # [embed_dim, vdim * num_heads]
+                                 attn_mask=None,  # [tgt_len,src_len] or [num_heads*batch_size,tgt_len, src_len]
+                                 ):
+    q = F.linear(query, q_proj_weight)
+    #  [tgt_len,batch_size, embed_dim] x [embed_dim,kdim * num_heads] = [tgt_len,batch_size,kdim * num_heads]
+
+    k = F.linear(key, k_proj_weight)
+    # [src_len, batch_size, embed_dim] x [embed_dim, kdim * num_heads] = [src_len, batch_size, kdim * num_heads]
+
+    v = F.linear(value, v_proj_weight)
+    # [src_len, batch_size, embed_dim] x [embed_dim, vdim * num_heads] = [src_len, batch_size, vdim * num_heads]
+    tgt_len, bsz, embed_dim = query.size()  # [tgt_len,batch_size, embed_dim]
+    src_len = key.size(0)
+    head_dim = embed_dim // num_heads  # num_heads * head_dim = embed_dim
+    scaling = float(head_dim) ** -0.5
+    q = q * scaling  # [query_len,batch_size,kdim * num_heads]
+
+    if attn_mask is not None:  # [tgt_len,src_len] or [num_heads*batch_size,tgt_len, src_len]
+        if attn_mask.dim() == 2:
+            attn_mask = attn_mask.unsqueeze(0)  # [1, tgt_len,src_len]
+            if list(attn_mask.size()) != [1, query.size(0), key.size(0)]:
+                raise RuntimeError('The size of the 2D attn_mask is not correct.')
+        elif attn_mask.dim() == 3:
+            if list(attn_mask.size()) != [bsz * num_heads, query.size(0), key.size(0)]:
+                raise RuntimeError('The size of the 3D attn_mask is not correct.')
+        # 现在 atten_mask 的维度就变成了3D
+
+    q = q.contiguous().view(tgt_len, bsz * num_heads, head_dim).transpose(0, 1)
+    # [batch_size * num_heads,tgt_len,kdim]
+    # 因为前面是num_heads个头一起参与的计算，所以这里要进行一下变形，以便于后面计算。 且同时交换了0，1两个维度
+    k = k.contiguous().view(-1, bsz * num_heads, head_dim).transpose(0, 1)  # [batch_size * num_heads,src_len,kdim]
+    v = v.contiguous().view(-1, bsz * num_heads, head_dim).transpose(0, 1)  # [batch_size * num_heads,src_len,vdim]
+    attn_output_weights = torch.bmm(q, k.transpose(1, 2))
+    # [batch_size * num_heads,tgt_len,kdim] x [batch_size * num_heads, kdim, src_len]
+    # =  [batch_size * num_heads, tgt_len, src_len]  这就num_heads个QK相乘后的注意力矩阵
+
+    if attn_mask is not None:
+        attn_output_weights += attn_mask  # [batch_size * num_heads, tgt_len, src_len]
+
+    if key_padding_mask is not None:
+        attn_output_weights = attn_output_weights.view(bsz, num_heads, tgt_len, src_len)
+        # 变成 [batch_size, num_heads, tgt_len, src_len]的形状
+        attn_output_weights = attn_output_weights.masked_fill(
+            key_padding_mask.unsqueeze(1).unsqueeze(2),  # 扩展维度，从[batch_size,src_len]变成[batch_size,1,1,src_len]
+            float('-inf'))  #
+        attn_output_weights = attn_output_weights.view(bsz * num_heads, tgt_len,
+                                                       src_len)  # [batch_size * num_heads, tgt_len, src_len]
+
+    attn_output_weights = F.softmax(attn_output_weights, dim=-1)  # [batch_size * num_heads, tgt_len, src_len]
+    attn_output_weights = F.dropout(attn_output_weights, p=dropout_p, training=training)
+    attn_output = torch.bmm(attn_output_weights, v)
+    # Z = [batch_size * num_heads, tgt_len, src_len]  x  [batch_size * num_heads,src_len,vdim]
+    # = # [batch_size * num_heads,tgt_len,vdim]
+    # 这就num_heads个Attention(Q,K,V)结果
+
+    attn_output = attn_output.transpose(0, 1).contiguous().view(tgt_len, bsz, embed_dim)
+    # 先transpose成 [tgt_len, batch_size* num_heads ,kdim]
+    # 再view成 [tgt_len,batch_size,num_heads*kdim]
+    attn_output_weights = attn_output_weights.view(bsz, num_heads, tgt_len, src_len)
+
+    Z = F.linear(attn_output, out_proj_weight, out_proj_bias)
+    # 这里就是多个z  线性组合成Z  [tgt_len,batch_size,embed_dim]
+
+    return Z, attn_output_weights.sum(dim=1) / num_heads  # average attention weights over heads
