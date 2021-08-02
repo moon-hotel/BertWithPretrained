afd86653c59464f2f2a28d37ecd9039981cee296
moon-hotel <moon-hotel@hotmail.com>
2021-08-02 19:40:04 +0800

    添加配置类测试用例

---
+++ test/test_BertConfig.py
@@ -0,0 +1,11 @@
+import sys
+
+sys.path.append('../')
+from model.BasicBert.BertConfig import BertConfig
+
+if __name__ == '__main__':
+    json_file = '../pretrained_model/config.json'
+    config = BertConfig.from_json_file(json_file)
+    print(config.hidden_size)
+    position_embedding_type = getattr(config, "position_embedding_type", "absolute")
+    print(position_embedding_type)
