ab4a65fd7e1ea0ec36cbd759af5d76275f1515c8
moon-hotel <moon-hotel@hotmail.com>
2021-08-04 20:00:13 +0800

    添加BertModel模块

--- model/BasicBert/Bert.py
+++ model/BasicBert/Bert.py
@@ -1,12 +1,25 @@
+import torch
+from torch.nn.init import normal_
 from .BertEmbedding import BertEmbeddings
 from .MyTransformer import MyMultiheadAttention
 import torch.nn as nn
+from .BertConfig import BertConfig
+import os
+from copy import deepcopy
 
 
-# class BertModel(nn.Module):
-#     def __init__(self, config):
-#         self.config = config
-#         self.embeddings = BertEmbeddings()
+def get_activation(activation_string):
+    act = activation_string.lower()
+    if act == "linear":
+        return None
+    elif act == "relu":
+        return nn.ReLU()
+    elif act == "gelu":
+        return nn.GELU()
+    elif act == "tanh":
+        return nn.Tanh()
+    else:
+        raise ValueError("Unsupported activation: %s" % act)
 
 
 class BertSelfAttention(nn.Module):
@@ -17,7 +30,7 @@ class BertSelfAttention(nn.Module):
 
     def __init__(self, config):
         super(BertSelfAttention, self).__init__()
-        self.multi_head_attention = MyMultiheadAttention(hidden_size=config.hidden_size,
+        self.multi_head_attention = MyMultiheadAttention(embed_dim=config.hidden_size,
                                                          num_heads=config.num_attention_heads,
                                                          dropout=config.attention_probs_dropout_prob)
 
@@ -42,18 +55,17 @@ class BertSelfAttention(nn.Module):
 class BertSelfOutput(nn.Module):
     def __init__(self, config):
         super().__init__()
-        self.dense = nn.Linear(config.hidden_size, config.hidden_size)
+        # self.dense = nn.Linear(config.hidden_size, config.hidden_size)
         self.LayerNorm = nn.LayerNorm(config.hidden_size)
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
 
     def forward(self, hidden_states, input_tensor):
         """
-        
         :param hidden_states: [src_len, batch_size, hidden_size]
         :param input_tensor: [src_len, batch_size, hidden_size]
-        :return: 
+        :return: [src_len, batch_size, hidden_size]
         """
-        hidden_states = self.dense(hidden_states)
+        # hidden_states = self.dense(hidden_states)  # [src_len, batch_size, hidden_size]
         hidden_states = self.dropout(hidden_states)
         hidden_states = self.LayerNorm(hidden_states + input_tensor)
         return hidden_states
@@ -66,8 +78,14 @@ class BertAttention(nn.Module):
         self.output = BertSelfOutput(config)
 
     def forward(self,
-                hidden_states,  # [src_len, batch_size, hidden_size]
-                attention_mask=None):  # [batch_size, src_len]
+                hidden_states,
+                attention_mask=None):
+        """
+
+        :param hidden_states: [src_len, batch_size, hidden_size]
+        :param attention_mask: [batch_size, src_len]
+        :return: [src_len, batch_size, hidden_size]
+        """
         self_outputs = self.self(hidden_states,
                                  hidden_states,
                                  hidden_states,
@@ -76,3 +94,181 @@ class BertAttention(nn.Module):
         # self_outputs[0] shape: [src_len, batch_size, hidden_size]
         attention_output = self.output(self_outputs[0], hidden_states)
         return attention_output
+
+
+class BertIntermediate(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.dense = nn.Linear(config.hidden_size, config.intermediate_size)
+        if isinstance(config.hidden_act, str):
+            self.intermediate_act_fn = get_activation(config.hidden_act)
+        else:
+            self.intermediate_act_fn = config.hidden_act
+
+    def forward(self, hidden_states):
+        """
+
+        :param hidden_states: [src_len, batch_size, hidden_size]
+        :return: [src_len, batch_size, intermediate_size]
+        """
+        hidden_states = self.dense(hidden_states)  # [src_len, batch_size, intermediate_size]
+        if self.intermediate_act_fn is None:
+            hidden_states = hidden_states
+        else:
+            hidden_states = self.intermediate_act_fn(hidden_states)
+        return hidden_states
+
+
+class BertOutput(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.dense = nn.Linear(config.intermediate_size, config.hidden_size)
+        self.LayerNorm = nn.LayerNorm(config.hidden_size)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+
+    def forward(self, hidden_states, input_tensor):
+        """
+
+        :param hidden_states: [src_len, batch_size, intermediate_size]
+        :param input_tensor: [src_len, batch_size, hidden_size]
+        :return: [src_len, batch_size, hidden_size]
+        """
+        hidden_states = self.dense(hidden_states)  # [src_len, batch_size, hidden_size]
+        hidden_states = self.dropout(hidden_states)
+        hidden_states = self.LayerNorm(hidden_states + input_tensor)
+        return hidden_states
+
+
+class BertLayer(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.bert_attention = BertAttention(config)
+        self.bert_intermediate = BertIntermediate(config)
+        self.bert_output = BertOutput(config)
+
+    def forward(self,
+                hidden_states,
+                attention_mask=None):
+        """
+
+        :param hidden_states: [src_len, batch_size, hidden_size]
+        :param attention_mask: [batch_size, src_len]
+        :return: [src_len, batch_size, hidden_size]
+        """
+        attention_output = self.bert_attention(hidden_states, attention_mask)
+        # [src_len, batch_size, hidden_size]
+        intermediate_output = self.bert_intermediate(attention_output)
+        # [src_len, batch_size, intermediate_size]
+        layer_output = self.bert_output(intermediate_output, attention_output)
+        # [src_len, batch_size, hidden_size]
+        return layer_output
+
+
+class BertEncoder(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+        self.bert_layers = nn.ModuleList([BertLayer(config) for _ in range(config.num_hidden_layers)])
+
+    def forward(
+            self,
+            hidden_states,
+            attention_mask=None):
+        """
+
+        :param hidden_states: [src_len, batch_size, hidden_size]
+        :param attention_mask: [batch_size, src_len]
+        :return:
+        """
+        all_encoder_layers = []
+        layer_output = hidden_states
+        for i, layer_module in enumerate(self.bert_layers):
+            layer_output = layer_module(layer_output,
+                                        attention_mask)
+            #  [src_len, batch_size, hidden_size]
+            all_encoder_layers.append(layer_output)
+        return all_encoder_layers
+
+
+class BertPooler(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.dense = nn.Linear(config.hidden_size, config.hidden_size)
+        self.activation = nn.Tanh()
+        self.config = config
+
+    def forward(self, hidden_states):
+        """
+
+        :param hidden_states:  [src_len, batch_size, hidden_size]
+        :return: [batch_size, hidden_size]
+        """
+        if self.config.pooler_type == "first_token_transform":
+            token_tensor = hidden_states[0, :].reshape(-1, self.config.hidden_size)
+        elif self.config.pooler_type == "all_token_average":
+            token_tensor = torch.mean(hidden_states, dim=0)
+        pooled_output = self.dense(token_tensor)  # [batch_size, hidden_size]
+        pooled_output = self.activation(pooled_output)
+        return pooled_output
+
+
+class BertModel(nn.Module):
+    """
+
+    """
+
+    def __init__(self, config):
+        super().__init__()
+        self.bert_embeddings = BertEmbeddings(config)
+        self.bert_encoder = BertEncoder(config)
+        self.bert_pooler = BertPooler(config)
+        self.config = config
+        self._reset_parameters()
+
+    def forward(self,
+                input_ids=None,
+                attention_mask=None,
+                token_type_ids=None,
+                position_ids=None):
+        """
+
+        :param input_ids:  [src_len, batch_size]
+        :param attention_mask: [batch_size, src_len]
+        :param token_type_ids: [batch_size, src_len]
+        :param position_ids: [1,src_len]
+        :return:
+        """
+        embedding_output = self.bert_embeddings(input_ids=input_ids,
+                                                position_ids=position_ids,
+                                                token_type_ids=token_type_ids)
+        all_encoder_outputs = self.bert_encoder(embedding_output,
+                                                attention_mask=attention_mask)
+        sequence_output = all_encoder_outputs[-1]  # 取最后一层
+        pooled_output = self.bert_pooler(sequence_output)
+        # 默认是最后一层的first token 即[cls]位置经dense + tanh 后的结果
+        return pooled_output, all_encoder_outputs
+
+    def _reset_parameters(self):
+        r"""Initiate parameters in the transformer model."""
+        """
+        初始化
+        """
+        for p in self.parameters():
+            if p.dim() > 1:
+                normal_(p, mean=0.0, std=self.config.initializer_range)
+
+    @classmethod
+    def from_pretrained(cls, pretrained_model_dir=None):
+        config_path = os.path.join(pretrained_model_dir, "config.json")
+        config = BertConfig.from_json_file(config_path)
+        model = cls(config)  # 初始化模型
+        pretrained_model_path = os.path.join(pretrained_model_dir, "pytorch_model.bin")
+        loaded_paras = torch.load(pretrained_model_path)
+        state_dict = deepcopy(model.state_dict())
+        loaded_paras_names = list(loaded_paras.keys())[:-8]
+        model_paras_names = list(state_dict.keys())[1:]
+        for i in range(len(loaded_paras_names)):
+            state_dict[model_paras_names[i]] = loaded_paras[loaded_paras_names[i]]
+            print(f"成功将参数{loaded_paras_names[i]}赋值给{model_paras_names[i]}")
+        model.load_state_dict(state_dict)
+        return model
