add17243b2359dc0f1a2d775b8a2ea684c502904
moon-hotel <moon-hotel@hotmail.com>
2021-08-10 08:29:28 +0800

    添加基于Bert的下游文本分类模块

---
+++ model/BertForClassification/BertForSequenceClassification.py
@@ -0,0 +1,41 @@
+from ..BasicBert.Bert import BertModel
+import torch.nn as nn
+
+
+class BertForSequenceClassification(nn.Module):
+    def __init__(self, config, num_labels, bert_pretrained_model_dir=None):
+        super(BertForSequenceClassification, self).__init__()
+        self.num_labels = num_labels
+        if bert_pretrained_model_dir is not None:
+            self.bert = BertModel.from_pretrained()
+        else:
+            self.bert = BertModel(config)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+        self.classifier = nn.Linear(config.hidden_size, num_labels)
+
+    def forward(self, input_ids,
+                attention_mask=None,
+                token_type_ids=None,
+                position_ids=None,
+                labels=None):
+        """
+
+        :param input_ids:  [src_len, batch_size]
+        :param attention_mask: [batch_size, src_len]
+        :param token_type_ids: 句子分类时为None
+        :param position_ids: [1,src_len]
+        :param labels: [batch_size,]
+        :return:
+        """
+        pooled_output, _ = self.bert(input_ids=input_ids,
+                                     attention_mask=attention_mask,
+                                     token_type_ids=token_type_ids,
+                                     position_ids=position_ids)
+        pooled_output = self.dropout(pooled_output)
+        logits = self.classifier(pooled_output)
+        if labels is not None:
+            loss_fct = nn.CrossEntropyLoss()
+            loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
+            return loss
+        else:
+            return logits
