0ad8492672d668b762514c22eab9a937f99b00f6
moon-hotel <moon-hotel@hotmail.com>
2021-10-01 14:57:43 +0800

    添加新参数

--- model/BasicBert/BertConfig.py
+++ model/BasicBert/BertConfig.py
@@ -7,16 +7,17 @@ class BertConfig(object):
     """Configuration for `BertModel`."""
 
     def __init__(self,
-                 vocab_size,
+                 vocab_size=21128,
                  hidden_size=768,
                  num_hidden_layers=12,
                  num_attention_heads=12,
                  intermediate_size=3072,
+                 pad_token_id = 0,
                  hidden_act="gelu",
                  hidden_dropout_prob=0.1,
                  attention_probs_dropout_prob=0.1,
                  max_position_embeddings=512,
-                 type_vocab_size=16,
+                 type_vocab_size=2,
                  initializer_range=0.02):
         """Constructs BertConfig.
         Args:
@@ -47,6 +48,7 @@ class BertConfig(object):
         self.num_attention_heads = num_attention_heads
         self.hidden_act = hidden_act
         self.intermediate_size = intermediate_size
+        self.pad_token_id=pad_token_id
         self.hidden_dropout_prob = hidden_dropout_prob
         self.attention_probs_dropout_prob = attention_probs_dropout_prob
         self.max_position_embeddings = max_position_embeddings
