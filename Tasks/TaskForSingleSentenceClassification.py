6feff897f638fcd1b4b32df20a40a1c05b7fbe7d
moon-hotel <moon-hotel@hotmail.com>
2021-10-25 20:29:42 +0800

    修改数据载入方法

--- Tasks/TaskForSingleSentenceClassification.py
+++ Tasks/TaskForSingleSentenceClassification.py
@@ -1,9 +1,9 @@
 import sys
 
 sys.path.append('../')
-from model.BertForClassification.BertForSingleSentenceClassification import BertForSingleSentenceClassification
+from model.BertForClassification.BertForSentenceClassification import BertForSentenceClassification
 from model.BasicBert.BertConfig import BertConfig
-from utils.data_helpers import LoadClassificationDataset
+from utils.data_helpers import LoadSingleSentenceClassificationDataset
 from utils.log_helper import Logger
 from transformers import BertTokenizer
 import logging
@@ -16,12 +16,12 @@ class ModelConfig:
     def __init__(self):
         self.project_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
         self.dataset_dir = os.path.join(self.project_dir, 'data', 'SingleSentenceClassification')
-        self.pretrained_model_dir = os.path.join(self.project_dir, "pretrained_model")
+        self.pretrained_model_dir = os.path.join(self.project_dir, "bert_base_chinese")
         self.vocab_path = os.path.join(self.pretrained_model_dir, 'vocab.txt')
         self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
         self.train_file_path = os.path.join(self.dataset_dir, 'toutiao_train.txt')
         self.val_file_path = os.path.join(self.dataset_dir, 'toutiao_val.txt')
-        self.test_file_path = os.path.join(self.dataset_dir, 'test.txt')
+        self.test_file_path = os.path.join(self.dataset_dir, 'toutiao_test.txt')
         self.model_save_dir = os.path.join(self.project_dir, 'cache')
         self.logs_save_dir = os.path.join(self.project_dir, 'logs')
         self.split_sep = '_!_'
@@ -47,9 +47,9 @@ class ModelConfig:
 
 
 def train(config):
-    classification_model = BertForSingleSentenceClassification(config,
-                                                               config.num_labels,
-                                                               config.pretrained_model_dir)
+    classification_model = BertForSentenceClassification(config,
+                                                         config.num_labels,
+                                                         config.pretrained_model_dir)
     model_save_path = os.path.join(config.model_save_dir, 'model.pt')
     if os.path.exists(model_save_path):
         loaded_paras = torch.load(model_save_path)
@@ -59,13 +59,13 @@ def train(config):
     optimizer = torch.optim.Adam(classification_model.parameters(), lr=5e-5)
     classification_model.train()
     bert_tokenize = BertTokenizer.from_pretrained(model_config.pretrained_model_dir).tokenize
-    data_loader = LoadClassificationDataset(vocab_path=config.vocab_path,
-                                            tokenizer=bert_tokenize,
-                                            batch_size=config.batch_size,
-                                            max_sen_len=config.max_sen_len,
-                                            split_sep=config.split_sep,
-                                            max_position_embeddings=config.max_position_embeddings,
-                                            pad_index=config.pad_token_id)
+    data_loader = LoadSingleSentenceClassificationDataset(vocab_path=config.vocab_path,
+                                                          tokenizer=bert_tokenize,
+                                                          batch_size=config.batch_size,
+                                                          max_sen_len=config.max_sen_len,
+                                                          split_sep=config.split_sep,
+                                                          max_position_embeddings=config.max_position_embeddings,
+                                                          pad_index=config.pad_token_id)
     train_iter, test_iter, val_iter = data_loader.load_train_val_test_data(config.train_file_path,
                                                                            config.val_file_path,
                                                                            config.test_file_path)
@@ -103,24 +103,24 @@ def train(config):
 
 
 def inference(config):
-    classification_model = BertForSingleSentenceClassification(config,
-                                                               config.num_labels,
-                                                               config.pretrained_model_dir)
+    classification_model = BertForSentenceClassification(config,
+                                                         config.num_labels,
+                                                         config.pretrained_model_dir)
     model_save_path = os.path.join(config.model_save_dir, 'model.pt')
     if os.path.exists(model_save_path):
         loaded_paras = torch.load(model_save_path)
         classification_model.load_state_dict(loaded_paras)
         config.logger.info("## 成功载入已有模型，进行预测......")
     classification_model = classification_model.to(config.device)
-    data_loader = LoadClassificationDataset(vocab_path=config.vocab_path,
-                                            tokenizer=BertTokenizer.from_pretrained(
-                                                config.pretrained_model_dir).tokenize,
-                                            batch_size=config.batch_size,
-                                            max_sen_len=config.max_sen_len,
-                                            split_sep=config.split_sep,
-                                            max_position_embeddings=config.max_position_embeddings,
-                                            pad_index=config.pad_token_id,
-                                            is_sample_shuffle=config.is_sample_shuffle)
+    data_loader = LoadSingleSentenceClassificationDataset(vocab_path=config.vocab_path,
+                                                          tokenizer=BertTokenizer.from_pretrained(
+                                                              config.pretrained_model_dir).tokenize,
+                                                          batch_size=config.batch_size,
+                                                          max_sen_len=config.max_sen_len,
+                                                          split_sep=config.split_sep,
+                                                          max_position_embeddings=config.max_position_embeddings,
+                                                          pad_index=config.pad_token_id,
+                                                          is_sample_shuffle=config.is_sample_shuffle)
     train_iter, test_iter, val_iter = data_loader.load_train_val_test_data(config.train_file_path,
                                                                            config.val_file_path,
                                                                            config.test_file_path)
@@ -144,4 +144,4 @@ def evaluate(data_iter, model, device):
 if __name__ == '__main__':
     model_config = ModelConfig()
     train(model_config)
-    # inference(model_config)
+    inference(model_config)
