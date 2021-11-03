182b40b665082c0be1059b0e95a4c3014d00d593
moon-hotel <moon-hotel@hotmail.com>
2021-11-03 19:29:11 +0800

    修改测试案例

--- test/test_BertForSentenceClassification.py
+++ test/test_BertForSentenceClassification.py
@@ -3,8 +3,15 @@ import sys
 sys.path.append('../')
 from model.DownstreamTasks.BertForSentenceClassification import BertForSentenceClassification
 from model.BasicBert.BertConfig import BertConfig
+import torch
 
 json_file = '../bert_base_chinese/config.json'
 config = BertConfig.from_json_file(json_file)
-model = BertForSentenceClassification(config, 10)
-print(model)
+config.__dict__['num_labels'] = 10
+model = BertForSentenceClassification(config)
+
+input_ids = torch.tensor([[1, 2, 3, 4], [5, 6, 7, 8]])
+attention_mask = torch.tensor([[1, 0], [1, 0], [1, 1], [1, 1]])
+logits = model(input_ids=input_ids,
+               attention_mask=attention_mask)
+print(logits.shape)
