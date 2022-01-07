4f97b63844daef31d86e31e6bb36c481a7b02881
moon-hotel <moon-hotel@hotmail.com>
2022-01-07 20:59:46 +0800

    添加模型评估模块

--- Tasks/TaskForPretraining.py
+++ Tasks/TaskForPretraining.py
@@ -24,18 +24,19 @@ class ModelConfig:
         self.test_file_path = os.path.join(self.dataset_dir, 'wiki.test.tokens')
         self.model_save_dir = os.path.join(self.project_dir, 'cache')
         self.logs_save_dir = os.path.join(self.project_dir, 'logs')
+        self.data_name = 'wiki2'
+        self.model_save_path = os.path.join(self.model_save_dir, f'model_{self.data_name}.pt')
         self.is_sample_shuffle = True
-        self.use_embedding_weight = False
+        self.use_embedding_weight = True
         self.batch_size = 16
-        self.max_sen_len = None
+        self.max_sen_len = None  # 为None时则采用每个batch中最长的样本对该batch中的样本进行padding
         self.pad_index = 0
         self.random_state = 2021
-        self.data_name = 'wiki2'
         self.learning_rate = 3.5e-5
         self.masked_rate = 0.15
         self.masked_token_rate = 0.8
         self.masked_token_unchanged_rate = 0.5
-        self.log_level = logging.INFO
+        self.log_level = logging.DEBUG
         self.use_torch_multi_head = False  # False表示使用model/BasicBert/MyTransformer中的多头实现
         self.epochs = 2
         self.model_val_per_epoch = 1
@@ -57,9 +58,8 @@ class ModelConfig:
 def train(config):
     model = BertForPretrainingModel(config,
                                     config.pretrained_model_dir)
-    model_save_path = os.path.join(config.model_save_dir, 'model.pt')
-    if os.path.exists(model_save_path):
-        loaded_paras = torch.load(model_save_path)
+    if os.path.exists(config.model_save_path):
+        loaded_paras = torch.load(config.model_save_path)
         model.load_state_dict(loaded_paras)
         logging.info("## 成功载入已有模型，进行追加训练......")
     model = model.to(config.device)
@@ -67,6 +67,7 @@ def train(config):
     bert_tokenize = BertTokenizer.from_pretrained(config.pretrained_model_dir).tokenize
     data_loader = LoadBertPretrainingDataset(vocab_path=config.vocab_path,
                                              tokenizer=bert_tokenize,
+                                             batch_size=config.batch_size,
                                              max_sen_len=config.max_sen_len,
                                              max_position_embeddings=config.max_position_embeddings,
                                              pad_index=config.pad_index,
@@ -102,26 +103,21 @@ def train(config):
             losses += loss.item()
             mlm_acc, _, _, nsp_acc, _, _ = accuracy(mlm_logits, nsp_logits, b_mlm_label,
                                                     b_nsp_label, data_loader.PAD_IDX)
-            if idx % 10 == 0:
+            if idx % 20 == 0:
                 logging.info(f"Epoch: {epoch}, Batch[{idx}/{len(train_iter)}], "
                              f"Train loss :{loss.item():.3f}, Train mlm acc: {mlm_acc:.3f},"
                              f"nsp acc: {nsp_acc:.3f}")
-            if idx % 100 == 0:
-                pass
         end_time = time.time()
         train_loss = losses / len(train_iter)
         logging.info(f"Epoch: {epoch}, Train loss: "
                      f"{train_loss:.3f}, Epoch time = {(end_time - start_time):.3f}s")
-        # if (epoch + 1) % config.model_val_per_epoch == 0:
-        #     acc, _ = evaluate(val_iter, model,
-        #                       config.device,
-        #                       data_loader.PAD_IDX,
-        #                       inference=False)
-        #     logging.info(f" ### Accuracy on val: {round(acc, 4)}")
-        #
-        #     if acc > max_acc:
-        #         max_acc = acc
-        #     torch.save(model.state_dict(), config.model_save_path)
+        if (epoch + 1) % config.model_val_per_epoch == 0:
+            mlm_acc, nsp_acc = evaluate(config, val_iter, model, data_loader.PAD_IDX)
+            logging.info(f" ### MLM Accuracy on val: {round(mlm_acc, 4)}, "
+                         f"NSP Accuracy on val: {round(nsp_acc, 4)}")
+            if mlm_acc > max_acc:
+                max_acc = mlm_acc
+                torch.save(model.state_dict(), config.model_save_path)
 
 
 def accuracy(mlm_logits, nsp_logits, mlm_labels, nsp_label, PAD_IDX):
@@ -147,7 +143,30 @@ def accuracy(mlm_logits, nsp_logits, mlm_labels, nsp_label, PAD_IDX):
     nsp_correct = (nsp_logits.argmax(1) == nsp_label).float().sum()
     nsp_total = len(nsp_label)
     nsp_acc = float(nsp_correct) / nsp_total
-    return mlm_acc, mlm_correct, mlm_total, nsp_acc, nsp_correct, nsp_total
+    return [mlm_acc, mlm_correct, mlm_total, nsp_acc, nsp_correct, nsp_total]
+
+
+def evaluate(config, data_iter, model, PAD_IDX):
+    model.eval()
+    mlm_corrects, mlm_totals, nsp_corrects, nsp_totals = 0, 0, 0, 0
+    with torch.no_grad():
+        for idx, (b_token_ids, b_segs, b_mask, b_mlm_label, b_nsp_label) in enumerate(data_iter):
+            b_token_ids = b_token_ids.to(config.device)  # [src_len, batch_size]
+            b_segs = b_segs.to(config.device)
+            b_mask = b_mask.to(config.device)
+            b_mlm_label = b_mlm_label.to(config.device)
+            b_nsp_label = b_nsp_label.to(config.device)
+            mlm_logits, nsp_logits = model(input_ids=b_token_ids,
+                                           attention_mask=b_mask,
+                                           token_type_ids=b_segs)
+            result = accuracy(mlm_logits, nsp_logits, b_mlm_label, b_nsp_label, PAD_IDX)
+            _, mlm_cor, mlm_tot, _, nsp_cor, nsp_tot = result
+            mlm_corrects += mlm_cor
+            mlm_totals += mlm_tot
+            nsp_corrects += nsp_cor
+            nsp_totals += nsp_tot
+    model.train()
+    return [float(mlm_corrects) / mlm_totals, float(nsp_corrects) / nsp_totals]
 
 
 if __name__ == '__main__':
