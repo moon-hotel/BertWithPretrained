bdc296b75fbf97ecbacdba366fad891af872da07
moon-hotel <moon-hotel@hotmail.com>
2022-01-16 20:20:27 +0800

    添加中文语料预处理测试模块

--- test/test_LoadBertPretrainingDataset.py
+++ test/test_LoadBertPretrainingDataset.py
@@ -11,12 +11,24 @@ from utils.create_pretraining_data import LoadBertPretrainingDataset
 class ModelConfig:
     def __init__(self):
         self.project_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-        self.dataset_dir = os.path.join(self.project_dir, 'data', 'WikiText')
-        self.pretrained_model_dir = os.path.join(self.project_dir, "bert_base_uncased_english")
+
+        # ========== wike2 数据集相关配置
+        # self.dataset_dir = os.path.join(self.project_dir, 'data', 'WikiText')
+        # self.pretrained_model_dir = os.path.join(self.project_dir, "bert_base_uncased_english")
+        # self.train_file_path = os.path.join(self.dataset_dir, 'wiki.train.tokens')
+        # self.val_file_path = os.path.join(self.dataset_dir, 'wiki.valid.tokens')
+        # self.test_file_path = os.path.join(self.dataset_dir, 'wiki.test.tokens')
+        # self.data_name = 'wiki2'
+
+        # ========== songci 数据集相关配置
+        self.dataset_dir = os.path.join(self.project_dir, 'data', 'SongCi')
+        self.pretrained_model_dir = os.path.join(self.project_dir, "bert_base_chinese")
+        self.train_file_path = os.path.join(self.dataset_dir, 'songci.train.txt')
+        self.val_file_path = os.path.join(self.dataset_dir, 'songci.valid.txt')
+        self.test_file_path = os.path.join(self.dataset_dir, 'songci.test.txt')
+        self.data_name = 'songci'
+
         self.vocab_path = os.path.join(self.pretrained_model_dir, 'vocab.txt')
-        self.train_file_path = os.path.join(self.dataset_dir, 'wiki.train.tokens')
-        self.val_file_path = os.path.join(self.dataset_dir, 'wiki.valid.tokens')
-        self.test_file_path = os.path.join(self.dataset_dir, 'wiki.test.tokens')
         self.model_save_dir = os.path.join(self.project_dir, 'cache')
         self.logs_save_dir = os.path.join(self.project_dir, 'logs')
         self.is_sample_shuffle = True
@@ -26,13 +38,12 @@ class ModelConfig:
         self.pad_index = 0
         self.is_sample_shuffle = True
         self.random_state = 2021
-        self.data_name = 'wiki2'
         self.masked_rate = 0.15
         self.masked_token_rate = 0.8
         self.masked_token_unchanged_rate = 0.5
         self.log_level = logging.DEBUG
 
-        logger_init(log_file_name='wiki', log_level=self.log_level,
+        logger_init(log_file_name=self.data_name, log_level=self.log_level,
                     log_dir=self.logs_save_dir)
         if not os.path.exists(self.model_save_dir):
             os.makedirs(self.model_save_dir)
@@ -62,10 +73,10 @@ if __name__ == '__main__':
         test_file_path=model_config.test_file_path,
         train_file_path=model_config.train_file_path,
         val_file_path=model_config.val_file_path)
-    for b_token_ids, b_segs, b_mask, b_mlm_label, b_nsp_label in test_iter:
-        print(b_token_ids.shape)  # [src_len,batch_size]
-        print(b_segs.shape)  # [src_len,batch_size]
-        print(b_mask.shape)  # [batch_size,src_len]
-        print(b_mlm_label.shape)  # [src_len,batch_size]
-        print(b_nsp_label.shape)  # [batch_size]
-        break
+    # for b_token_ids, b_segs, b_mask, b_mlm_label, b_nsp_label in test_iter:
+    #     print(b_token_ids.shape)  # [src_len,batch_size]
+    #     print(b_segs.shape)  # [src_len,batch_size]
+    #     print(b_mask.shape)  # [batch_size,src_len]
+    #     print(b_mlm_label.shape)  # [src_len,batch_size]
+    #     print(b_nsp_label.shape)  # [batch_size]
+    #     break
