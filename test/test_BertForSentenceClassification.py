9fd46e6d4a59b4d0c48b1dd5ac9cee3d64e73bf1
moon-hotel <moon-hotel@hotmail.com>
2022-07-09 21:02:52 +0800

    添加注释

--- test/test_BertForSentenceClassification.py
+++ test/test_BertForSentenceClassification.py
@@ -10,8 +10,8 @@ config = BertConfig.from_json_file(json_file)
 config.__dict__['num_labels'] = 10
 model = BertForSentenceClassification(config)
 
-input_ids = torch.tensor([[1, 2, 3, 4], [5, 6, 7, 8]])
-attention_mask = torch.tensor([[1, 0], [1, 0], [1, 1], [1, 1]])
+input_ids = torch.tensor([[1, 2, 3, 4], [5, 6, 7, 8]])  # [src_len,batch_size]
+attention_mask = torch.tensor([[1, 0], [1, 0], [1, 1], [1, 1]])  # [batch_size,src_len]
 logits = model(input_ids=input_ids,
                attention_mask=attention_mask)
 print(logits.shape)
