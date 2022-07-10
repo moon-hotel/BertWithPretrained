32107f0921dd09fbca77d10e60f55dd4574e40c1
moon-hotel <moon-hotel@hotmail.com>
2022-07-10 10:36:50 +0800

    添加NERDataset模块

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,5 +2,6 @@ from .data_helpers import LoadSingleSentenceClassificationDataset
 from .data_helpers import LoadMultipleChoiceDataset
 from .data_helpers import LoadPairSentenceClassificationDataset
 from .data_helpers import LoadSQuADQuestionAnsweringDataset
+from .data_helpers import LoadChineseNERDataset
 from .log_helper import logger_init
 from .create_pretraining_data import LoadBertPretrainingDataset
