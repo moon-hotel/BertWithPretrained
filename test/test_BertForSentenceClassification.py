905e2eeb669a08390793cb53ac9267db535261b9
nulls8 <moon-hotel@hotmail.com>
2025-02-28 22:06:08 +0800

    修订代码

--- test/test_BertForSentenceClassification.py
+++ test/test_BertForSentenceClassification.py
@@ -4,7 +4,7 @@ sys.path.append('../')
 from model.DownstreamTasks.BertForSentenceClassification import BertForSentenceClassification
 from model.BasicBert.BertConfig import BertConfig
 import torch
-from torch.utils.tensorboard import SummaryWriter
+# from torch.utils.tensorboard import SummaryWriter
 
 if __name__ == '__main__':
     json_file = '../bert_base_chinese/config.json'
@@ -14,12 +14,12 @@ if __name__ == '__main__':
     model = BertForSentenceClassification(config)
 
     input_ids = torch.tensor([[1, 2, 3, 4], [5, 6, 7, 8]])  # [src_len,batch_size]
-    attention_mask = torch.tensor([[1, 0], [1, 0], [1, 1], [1, 1]])  # [batch_size,src_len]
+    attention_mask = torch.tensor([[True, False], [True, False], [True, True], [True, True]])  # [batch_size,src_len]
     logits = model(input_ids=input_ids,
                    attention_mask=attention_mask)
     print(logits.shape)
-    writer = SummaryWriter('./runs')
-    writer.add_graph(model, input_ids)
+    # writer = SummaryWriter('./runs')
+    # writer.add_graph(model, input_ids)
     # tensorboard 使用介绍见： https://mp.weixin.qq.com/s/8NB_hHYBq072xrSuCtgnEw
     #                       https://zhuanlan.zhihu.com/p/484289017
 
